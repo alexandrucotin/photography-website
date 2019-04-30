a87b842d7b660648a8d3d0b0d81ac737daa5ab83
alex cotin <a.cotin96@yahoo.com>
2019-04-30 19:01:42 +0200

    changed me page

--- src/components/Me.js
+++ src/components/Me.js
@@ -4,6 +4,7 @@ import Grid from "@material-ui/core/Grid";
 import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 import image from "../static/me.jpg";
 import SocialBar from "./SocialBar";
+import { Typography } from "@material-ui/core";
 
 class Me extends React.Component {
   constructor(props) {
@@ -41,34 +42,33 @@ class Me extends React.Component {
         {this.state.isVisible ? (
           <div className="me">
             <SocialBar />
-            <div className="div-title-me">
-              <Grid container className="center-div">
-                <Grid item className="divide">
-                  <img
-                    src={image}
-                    alt="alex cotin"
-                    className="responsive-me round"
-                  />
-                </Grid>
-                <Grid item className="divide">
-                  <h1 className="title-me">About me</h1>
-                  <p className="text-me">
-                    Hi, My name is Alex, and I currently study Computer Science
-                    at Verona, with a huge passion for photography, film making
-                    and coding obviously. I've practice 2 years in photography &
-                    filmaking field doing video and photo shooting as for
-                    writing code, I started when I was 16 years old. I've lived
-                    in Verona for the past 13 years and had time to discover the
-                    most interesting places to see and the best places to eat
-                    and drink. Even if you are not comfortable in front of the
-                    camera there is no problem because I like to capture things
-                    in the most natural way. I can speak fluently English,
-                    Italian and Romanian. So communication shouldn't be a
-                    problem. :)
-                  </p>
-                </Grid>
+            <Grid container className="center-div div-title-me">
+              <Grid item xs={12} sm={12} lg={3} xl={3}>
+                <img
+                  src={image}
+                  alt="alex cotin"
+                  className="responsive-me round"
+                />
               </Grid>
-            </div>
+              <Grid item xs={12} sm={12} lg={3} xl={3}>
+                <Typography component="h2" variant="h1" gutterBottom>
+                  About me
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  Hi, My name is Alex, and I currently study Computer Science at
+                  Verona, with a huge passion for photography, film making and
+                  coding obviously. I've practice 2 years in photography &
+                  filmaking field doing video and photo shooting as for writing
+                  code, I started when I was 16 years old. I've lived in Verona
+                  for the past 13 years and had time to discover the most
+                  interesting places to see and the best places to eat and
+                  drink. Even if you are not comfortable in front of the camera
+                  there is no problem because I like to capture things in the
+                  most natural way. I can speak fluently English, Italian and
+                  Romanian. So communication shouldn't be a problem. :)
+                </Typography>
+              </Grid>
+            </Grid>
             <BottomAppBar />
           </div>
         ) : null}
